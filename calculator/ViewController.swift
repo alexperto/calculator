04cdb5d38a6299185a100419f308d81f4e8ac08d
Alex Cabrera <alexperto@gmail.com>
2015-03-16 22:00:16 -0700

    Lecture 2: perform operations

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -24,5 +24,52 @@ class ViewController: UIViewController {
         }
         println("append: \(digit)")
     }
+    
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        isUserInTheMiddleOfTypping = false
+        operandStack.append(displayValue)
+        println("stack: \(operandStack)")
+    }
+    
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            isUserInTheMiddleOfTypping = false
+        }
+    }
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isUserInTheMiddleOfTypping{
+            enter()
+        }
+        switch operation{
+          case "+": performOperation{ $0 + $1 }
+          case "−": performOperation{ $1 - $0 }
+          case "×": performOperation{ $0 * $1 }
+          case "÷": performOperation{ $1 / $0 }
+          case "√": performOperation{ sqrt($0) }
+          default : break
+        }
+    }
+    
+    func performOperation(operation:(Double) -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation:(Double, Double) -> Double){
+        if operandStack.count >= 2 {
+          displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+          enter()
+        }
+    }
+
 }
 
